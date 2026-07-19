5939248f215955e828e83b74b7ff3317abfe9a36
PathetiX <pow3r0807@gmail.com>
2026-07-19 19:55:32 +0300

    test(config): clean serialization imports

--- core/config/src/test/kotlin/pw/x4/ninety/core/config/SingBoxConfigBuilderTest.kt
+++ core/config/src/test/kotlin/pw/x4/ninety/core/config/SingBoxConfigBuilderTest.kt
@@ -1,11 +1,9 @@
 package pw.x4.ninety.core.config
 
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.boolean
-import kotlinx.serialization.json.content
 import kotlinx.serialization.json.int
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
