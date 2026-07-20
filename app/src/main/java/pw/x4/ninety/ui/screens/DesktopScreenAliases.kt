13939779cee0cba086451ccfbd29d70c0482a731
PathetiX <pow3r0807@gmail.com>
2026-07-20 10:21:28 +0300

    refactor(design): route app to desktop Profiles and Nodes

---
+++ app/src/main/java/pw/x4/ninety/ui/screens/DesktopScreenAliases.kt
@@ -0,0 +1,10 @@
+package pw.x4.ninety.ui.screens
+
+import androidx.compose.runtime.Composable
+import pw.x4.ninety.ui.layout.NinetyLayoutMetrics
+
+@Composable
+fun ProfilesScreen(metrics: NinetyLayoutMetrics) = DesktopProfilesScreen(metrics)
+
+@Composable
+fun NodesScreen(metrics: NinetyLayoutMetrics) = DesktopNodesScreen(metrics)
